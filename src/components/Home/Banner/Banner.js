8ee34a2ed183d240c6d2da2c19f228cfe9415311
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-05-10 02:32:52 +0600

    slider fixed

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,43 +1,55 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import { BsFillArrowRightCircleFill } from 'react-icons/bs';
-import banner from "../../../images/banner/banner.png";
-import banner1 from "../../../images/banner/banner1.png";
-import banner2 from "../../../images/banner/banner2.png";
-import './Banner.css';
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import "./Banner.css";
+
 const Banner = () => {
   return (
     <div className="banner-section flex-wrap d-flex justify-content-around align-items-center mb-3">
-        <div className="banner-left">
-            <span>Healthy and Delicious Juices</span>
-            <h2>Fulfill All Your Vitamin Needs By Drinking Juice</h2>
-          <p>Get freshness delivered on your doorstep</p>
-            <button>Get started <BsFillArrowRightCircleFill/></button>
-        </div>
-        <div className="banner-right ">
+      <div className="banner-left">
+        <span>Healthy and Delicious Juices</span>
+        <h2>Fulfill All Your Vitamin Needs By Drinking Juice</h2>
+        <button>
+          Get started <BsFillArrowRightCircleFill />
+        </button>
+      </div>
+      <div className="banner-right ">
         <Carousel>
-      <Carousel.Item>
-        <img className="d-block w-100" src={banner} alt="First slide" />
-        <Carousel.Caption>
-          <p className="carousel">Organic and Fresh Juice</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={banner1} alt="Second slide" />
-
-        <Carousel.Caption>
-        <p className="carousel">Organic and Fresh Juice</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img className="d-block w-100" src={banner2} alt="Third slide" />
-
-        <Carousel.Caption>
-        <p className="carousel">Organic and Fresh Juice</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    </div>
+          <Carousel.Item interval={1000} className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="https://images.unsplash.com/photo-1622597468666-27cb9cae0e45?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>ORGANIC AND FRESH FOOD</h3>
+              <p>Get freshness delivered on your doorstep</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={500} className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="https://images.unsplash.com/photo-1622597468620-656aa1f981ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>ORGANIC AND FRESH FOOD</h3>
+              <p>Get freshness delivered on your doorstep</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item className="carousel-item">
+            <img
+              className="d-block w-100"
+              src="https://images.unsplash.com/photo-1622597467821-df79dcb4f94d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>ORGANIC AND FRESH FOOD</h3>
+              <p>Get freshness delivered on your doorstep</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </div>
   );
 };
